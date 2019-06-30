d032d2298abfcdae60c319b52bd2550108379b4a
Priyanshu Tyagi <priyanshumain@gmail.com>
2019-06-30 23:05:58 +0530

    About Section

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {  } from 'react-bootstrap';
+import profilepic from '../img/my.jpg';
 
 const About = () => {
     return(
         <div>
-        <h1>This is about Priyanshu Tyagi</h1>
+        <article class="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+  <div class="tc">
+    <img src={profilepic} class="br-100 h6 w4 dib ba b--black-05 pa2" title="Photo of a kitty staring at you" />
+    <h1 class="f3 mb2">Priyanshu Tyagi</h1>
+    <h2 class="f5 fw4 gray mt0">Junior Fullstack Developer</h2>
+  </div>
+</article>
         </div>
     );
 };
